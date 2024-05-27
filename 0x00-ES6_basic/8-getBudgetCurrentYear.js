dbab471da2ed64147ea30a0335af4e055dfdc252
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-28 01:12:56 +0300

    Computed properties, use []

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,14 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+
+  budget[getCurrentYear['income']] = income;
+  budget[getCurrentYear['gdp']] = gdp;
+  budget[getCurrentYear['capita']] = capita;
+
+  return budget;
+}
