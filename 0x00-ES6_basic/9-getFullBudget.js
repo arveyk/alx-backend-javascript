20e8d2f68d197c52920ecd880539e00edde8430f
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-28 01:27:21 +0300

    semistandard linter

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,15 +1,15 @@
 import getBudgetObject from './7-getBudgetObject.js';
 
-export default function getFullBudgetObject(income, gdp, capita) {
+export default function getFullBudgetObject (income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(income) {
+    getIncomeInDollars (income) {
       return `$${income}`;
     },
-    getIncomeInEuros(income) {
+    getIncomeInEuros (income) {
       return `${income} euros`;
-    },
+    }
   };
 
   return fullBudget;
