8f2960a379412ab9cf5f88f7a9cb51b0bf6c7776
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-27 22:46:54 +0300

    block scoping

---
+++ 0x00-ES6_basic/1-block-scope.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  {
+    var task = false:
+    var task2 = true;
+  }
+
+  if (trueOrFalse {
+    var task = true;
+    var task2 = false;
+  }
+}
---
+++ 0x00-ES6_basic/1-main.js
@@ -0,0 +1,4 @@
+import taskBlock from './1-block-scope.js';
+
+console.log(taskBlock(true));
+console.log(taskBlock(false));
