b06b7dc1a8854d03e8a7a745ebbbb3d2e7a7f19b
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-28 00:59:44 +0300

    number of lines returned to original

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,8 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita
+  };
   return budget;
 }
