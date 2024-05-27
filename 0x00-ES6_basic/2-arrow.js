1c2de52f52bbaa762b94a843b92c00f6f0b5404d
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-27 23:48:18 +0300

    add function replaced with arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,8 @@
-export default () => {
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = function add(newNeighborhood){
+  this.addNeighborhood = (newNeighborhood) => {
   self.sanFranciscoNeighborhoods.push(newNeighborhood);
   return self.sanFranciscoNeighborhoods;
   };
