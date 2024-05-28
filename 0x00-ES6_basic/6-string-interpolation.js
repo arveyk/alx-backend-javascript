d5ea42bc470572718cd5492e6daf9f5e7abd67ef
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-28 20:23:34 +0300

    js linter

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -3,7 +3,7 @@ export default function getSanFranciscoDescription() {
   const budget = {
     income: '$119,868',
     gdp: '$154.3 billion',
-    capita: '$178,479'
+    capita: '$178,479',
   };
   str = `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
   return str;
