76683436ff934b4b0e58f953a9113dfe4deb533c
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-29 22:30:36 +0300

    exports

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,6 @@
-let handleResponseFromAPI = function(promise) {
+exports.handleResponseFromAPI = function(promise) {
   promise.then(function(result) {
     promise.resolve(result);
     console.log('Got a response from the API');
   }).catch(Error());
 };
-exports.handleResponseFromAPI = handleResponseFromAPI = ; 
