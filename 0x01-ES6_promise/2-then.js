53323f3edf6a97c3c17f95f87ab6631a025a40f8
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-29 22:08:45 +0300

    module export method

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,7 @@
-exports.handleResponseFromAPI = function(promise) {
+let handleResponseFromAPI = function(promise) {
   promise.then(function(result) {
     promise.resolve(result);
     console.log('Got a response from the API');
   }).catch(Error());
 };
+exports.handleResponseFromAPI = handleResponseFromAPI = ; 
