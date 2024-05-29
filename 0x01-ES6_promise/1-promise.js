3cded8fe2ab6bf94df83940dff5522b45914f1c4
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-29 21:10:41 +0300

    export a promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success == true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    }
+    else {
+      reject(Error("The fake API is not working currently"));
+    }
+  });
+}
+
+module.exports.getFullResponseFromAPI = getFullResponseFromAPI;
