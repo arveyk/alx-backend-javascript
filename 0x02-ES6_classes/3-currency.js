55c7a8b986a202144295b4598fcd453c2148806f
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-30 21:13:41 +0300

    class methods

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(codejs) {
+    this._code = codejs;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(namejs) {
+    this._name = namejs;
+  }
+
+  displayFullCurrency() {
+    return `${this._name}(${this._code})`;
+  }
+}
