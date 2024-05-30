1015e0603dfd763a122ed5faf0cbb97273fdbf3e
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-31 00:21:19 +0300

    sky inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from "./5-building.js";
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floor) {
+    super(sqft);
+    this._floor = floor;
+  }
+
+  get floor() {
+    return this._floor;
+  }
+
+  set floor(floorMeasure) {
+    if (!isNaN(floorMeasure))
+	  this._floor = floorMeasure;
+  }
+
+  valuationWarningMessage() {
+    super.valuationWarningMessage();
+    return `Evacuate slowly the ${this._floor} floors.`;
+  }
+}
