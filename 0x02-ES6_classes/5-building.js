f389f3df225e61cfc6f10b36158e58762eebea53
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-30 23:53:49 +0300

    abstract class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (!this.evaluationWarningMessage)
+	  throw new Error("Class extending Building must override evaluationWarningMessage");
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqftNum) {
+    this._sqft = Number(sqftNum);
+  }
+
+  evaluationWarningMessage() {
+  }
+}
