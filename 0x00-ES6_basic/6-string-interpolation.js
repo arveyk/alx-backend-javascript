875abf6a463276cf1b559b55ce9f93a2793411ab
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-31 16:37:41 +0300

    define variable

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -5,6 +5,6 @@ export default function getSanFranciscoDescription() {
     gdp: '$154.3 billion',
     capita: '$178,479',
   };
-  str = `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+  const str = `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
   return str;
 }
