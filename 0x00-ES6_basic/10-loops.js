39e630ca8a95b616e64f99e33997f7546ee2f691
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-31 16:53:55 +0300

    D

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx of array) {
-    let value = array[idx];
+  for (const idx of array) {
+    const value = array[idx];
     array[idx] = appendString + value;
   }
-
   return array;
 }
