4966a6b5c5e7eed6dc8bef4f7e74d8bc61b5696b
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-31 15:24:34 +0300

    check if value is of specified type

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,31 +1,37 @@
 export default class HolbertonCourse {
-  constructor(name, length, students){
+  constructor(name, length, students) {
     this._name = name;
     this._length = length;
-    this._student = student;
+    this._student = students;
   }
+
   get name() {
     return this._name;
   }
+
   set name(nameStr) {
-    if (typeof(nameStr) === String) {
-      this._name = nameStr;
+    if (typeof nameStr === 'string') {
+      this._name = String(nameStr);
     }
   }
+
   get length() {
     return this._length;
   }
+
   set length(length) {
-    if (!isNaN(length)) {
-      this._length = length
+    if (isNumber(length)) {
+      this._length = Number(length);
     }
   }
+
   get students() {
-    return this._students
+    return this._students;
   }
+
   set students(studentsN) {
-    if (typeof(studentN) === Array) {
-      this._students = studentN;
+    if (typeof(studentN) === [1, 2]) {
+      this._students = Array(studentN);
     }
   }
 }
