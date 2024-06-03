fdd181699bc16ba07222bd63d051597a9ad0689b
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-03 19:39:56 +0300

    firts commit, data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+export default function getListStudents() {
+  const students = [
+    {
+      firstName: "Guillaume",
+      id: 1,
+      location: "San Francisco"
+    },
+    {
+      firstName: "James",
+      id: 2,
+      location: "Columbia",
+      },
+    {
+      firstName: "Serena",
+      id: 5,
+      location: "San Francisco"
+    },
+  ];
+  return students;
+}
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,6 @@
+<-- JavaScipt data Manipulation -->
+
+In this project we are looking at 
+- how to use map, filter and reduce on arrays 
+- typed arrays
+- set map and weaklink data structures
