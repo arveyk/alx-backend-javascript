5e5f10437cda6d7287e1a8cb636ecd4982aa2cf6
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-05 00:02:41 +0300

    object attributes in right order

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,18 @@
 export default function getListStudents() {
   return [
     {
-      firstName: "Guillaume",
       id: 1,
+      firstName: "Guillaume",
       location: "San Francisco",
     },
     {
-      firstName: "James",
       id: 2,
+      firstName: "James",
       location: "Columbia",
       },
     {
-      firstName: "Serena",
       id: 5,
+      firstName: "Serena",
       location: "San Francisco",
     },
   ];
