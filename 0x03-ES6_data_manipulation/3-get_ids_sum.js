b0f5ffe1927c429cb5e073006e0ee2f0e84c8506
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-05 01:12:24 +0300

    initial value reinstated

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function(listStuds) {
-  return listStuds.reduce((sumVal, listStuds) => sumVal + listStuds.id);
+  return listStuds.reduce((sumVal, listStuds) => sumVal + listStuds.id, 0);
 }
