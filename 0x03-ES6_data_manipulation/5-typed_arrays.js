0353b83b2d40c0f0cd914a7f19cc7bca3f00c044
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-04 17:40:01 +0300

    function name included

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
-export default function(length, position, value) {
+export default function createInt8TypedArray(length, position, value) {
   const numCheck1 = Number.isInteger(length);
   const numCheck2 = Number.isInteger(position);
   const numCheck3 = Number.isInteger(value);
   if (numCheck1 || numCheck2 || numCheck3) {
-    throw new Error("Position outside range");
+    throw new Error('Position outside range');
   }
   const buff = new ArrayBuffer(length);
   const int8View = new Int8Array(buff);
