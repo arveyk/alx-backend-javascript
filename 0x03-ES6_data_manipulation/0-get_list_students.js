5c1502f522d6a65d85d5a0e89c9fcf0118916877
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-05 00:25:06 +0300

    location attribute

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -3,17 +3,17 @@ export default function getListStudents() {
     {
       id: 1,
       firstName: 'Guillaume',
-      location: 'San Francisco'
+      'location': 'San Francisco'
     },
     {
       id: 2,
       firstName: 'James',
-      location: 'Columbia'
+      'location': 'Columbia'
     },
     {
       id: 5,
       firstName: 'Serena',
-      location: 'San Francisco'
+      'location': 'San Francisco'
     },
   ];
 }
