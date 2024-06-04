d540e8068adfe07e4bfbd1547144993b552fbd60
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-04 18:07:22 +0300

    return value for map

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,10 @@
-export default function(listStudents, city, newGrade) {
+export default function updateStudentGradeByCity(listStudents, city, newGrade) {
   return listStudents.filter((listStudents) => listStudents.location === city)
     .map((listStudents) => {
       if (newGrade.studentId === listStudents.id) {
         listStudents.grade = newGrade.grade;
       }
-      listStudents.grade = "N/A";
+      listStudents.grade = 'N/A';
+      return listStudents;
     });
 }
--- 0x03-ES6_data_manipulation/4-upgrade_grade_by_city.js
+++
@@ -1,9 +0,0 @@
-export default function(listStudents, city, newGrade) {
-  return listStudents.filter((listStudents) => listStudents.location === city)
-    .map((listStudents) => {
-      if (newGrade.studentId === listStudents.id) {
-        listStudents.grade = newGrade.grade;
-      }
-      listStudents.grade = "N/A";
-    });
-}
