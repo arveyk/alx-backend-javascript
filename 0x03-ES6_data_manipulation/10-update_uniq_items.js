07cd942f7dd0d15ada5f6d6c5c5a8aa589b9b803
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-04 17:30:35 +0300

    js linter

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,11 @@
 export default function updateUniqueItems(map) {
-  if (!map instanceof Map) {
-	  throw new Error("Cannot process");
-  }
-  for (const [key, value] of map) {
-    if (value === 1) {
-      map.set(`${key}`, 100);
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  } else {
+    for (const [key, value] of map) {
+      if (value === 1) {
+        map.set(`${key}`, 100);
+      }
     }
   }
 }
