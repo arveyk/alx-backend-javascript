25df822322a2587a002f7f5f1188dc4622fc1cae
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-05 00:05:57 +0300

    js linter, semistandard linter

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,18 +2,18 @@ export default function getListStudents() {
   return [
     {
       id: 1,
-      firstName: "Guillaume",
-      location: "San Francisco",
+      firstName: 'Guillaume',
+      location: 'San Francisco'
     },
     {
       id: 2,
-      firstName: "James",
-      location: "Columbia",
-      },
+      firstName: 'James',
+      location: 'Columbia'
+    },
     {
       id: 5,
-      firstName: "Serena",
-      location: "San Francisco",
-    },
+      firstName: 'Serena',
+      location: 'San Francisco'
+    }
   ];
 }
