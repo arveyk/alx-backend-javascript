c96efd96763921336186407ff085d5f43f86feb3
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-04 17:32:46 +0300

    js linter

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,9 @@
 export default function groceriesList() {
   const greensMap = new Map();
-  greensMap.set("Apples", 10);
-  greensMap.set("Tomatoes", 10);
-  greensMap.set("Pasta", 1);
-  greensMap.set("Rice", 1);
-  greensMap.set("Banana", 5);
+  greensMap.set('Apples', 10);
+  greensMap.set('Tomatoes', 10);
+  greensMap.set('Pasta', 1);
+  greensMap.set('Rice', 1);
+  greensMap.set('Banana', 5);
   return greensMap;
 }
