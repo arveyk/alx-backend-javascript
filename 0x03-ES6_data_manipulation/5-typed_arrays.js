ade46778cbeaf7af178755924bed2ae6518e2425
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-04 17:36:43 +0300

    filename correction

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+export default function(length, position, value) {
+  const numCheck1 = Number.isInteger(length);
+  const numCheck2 = Number.isInteger(position);
+  const numCheck3 = Number.isInteger(value);
+  if (numCheck1 || numCheck2 || numCheck3) {
+    throw new Error("Position outside range");
+  }
+  const buff = new ArrayBuffer(length);
+  const int8View = new Int8Array(buff);
+  int8View[`${position}`] = value;
+  return int8View;
+}
