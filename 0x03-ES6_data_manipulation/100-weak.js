c6b7a525a209e0af46d32bc8e42e460e87c86694
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-04 15:53:13 +0300

    weakMaps tracking

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+const weakMap = new WeakMap();
+const queryAPI = function(endPoint) {
+  this.count = 0;
+  endPoint.forEach(function(endPoint) {
+    endPoint.count += 1;
+    if (endPoint.count >= 5) {
+      throw new Error("Endpoint load is high");
+    }
+  });
+}
+
+export {weakMap, queryAPI};
