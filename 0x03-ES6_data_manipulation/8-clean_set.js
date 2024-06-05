ffdc8c44e382644350e64c5be5a0ede8529e2ada
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-05 11:24:20 +0300

    slice less the startsWith string length

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   const strArray = [];
   for (const item of set) {
     if (item.startsWith(startString)) {
-      strArray.push(item[startString.length]);
+      strArray.push(item[startString.length - 1]);
     }
   }
   return strArray.join('-');
