84f48b5c1c133e8fe994aaffec968ea248934a19
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-05 11:31:22 +0300

    js linter

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,10 @@
 const weakMap = new WeakMap();
-const queryAPI = function(endPoint) {
+const queryAPI = function query(endPoint) {
   this.count = 0;
-  endPoint.forEach(function(endPoint) {
+  endPoint.forEach((endPoint) => {
     endPoint.count += 1;
     if (endPoint.count >= 5) {
-      throw new Error("Endpoint load is high");
+      throw new Error('Endpoint load is high');
     }
   });
 };
