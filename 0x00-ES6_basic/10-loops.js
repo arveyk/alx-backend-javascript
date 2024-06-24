02cc1cd02fee003c211f4ca76751006727a1fe3d
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-24 16:40:56 +0300

    creat a different array if for-of is in use

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx.aaa of array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
+  const arr = []
+  let count = 0
+  for (const idx of array) {
+    arr.push(`${appendString}${idx}`);
   }
+  array = arr;
   return array;
 }
