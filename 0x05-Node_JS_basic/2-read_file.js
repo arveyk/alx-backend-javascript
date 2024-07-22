6a1fcb177a471d5d718d0bd59ca1c2916a7435de
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-22 21:04:38 +0300

    Synchronous file reading, Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  let data = ""
+  try {
+     data = fs.readFileSync(path).toLocaleString();
+  } catch (err) {
+    throw new Error("Cannot load the database");
+  }
+  const rows = data.split("\n");
+
+  let count = 0;
+  const csList = [];
+  const sweList = [];
+
+  rows.forEach((row) => {
+    if (row.length === 0) {
+       return;
+    }
+    count += 1;
+    columns = row.split(',');
+    if (columns[3] === "CS") {
+      csList.push(columns[0]);
+    }
+    if (columns[3] === "SWE") {
+      sweList.push(columns[0]);
+    }
+  });
+  console.log("Number of students:", count - 1)
+  console.log(`Number of students in CS: ${csList.length}. List: ${csList.join(", ")}`);
+  console.log("Number of students in SWE: " +
+	  `${sweList.length}. List: ` + `${sweList.join(", ")}`);
+};
+
+module.exports = countStudents;
