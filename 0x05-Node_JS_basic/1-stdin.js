118e567a86ec2f4d78ab8acfc1b874336e704b10
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-23 11:28:49 +0300

    Semistandard

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdin.setEncoding('utf-8');
 process.stdout.write('Welcome to Holberton School, what is your name?' + '\n');
 process.stdin.on('readable', () => {
-  let name = process.stdin.read();
+  const name = process.stdin.read();
   if (name !== null) {
     process.stdout.write("Your name is: " + name);
   }
