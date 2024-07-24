f6de1acea723782b864a5bf35f0f80c87898b380
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-24 17:36:07 +0300

    Correct module import

--- 0x05-Node_JS_basic/3-main.js
+++ 0x05-Node_JS_basic/3-main.js
@@ -1,4 +1,4 @@
-const countStudents = require('/3-read_file_async');
+const countStudents = require('./3-read_file_async');
 
 countStudents("nope.csv")
 	.then(() => {
--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,13 +1,13 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 
 async function countStudents(path) {
-  let data = ""
+  let data = ''
   try {
-     data = await fs.readFile(path, { encoding: 'utf-8' });
+     data = await fs.promises.readFile(path, { encoding: 'utf-8' });
   } catch (err) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
-  const rows = data.split("\n");
+  const rows = data.split('\n');
 
   let count = 0;
   const csList = [];
@@ -19,17 +19,16 @@ async function countStudents(path) {
     }
     count = count + 1;
     let columns = row.split(',');
-    if (columns[3] === "CS") {
+    if (columns[3] === 'CS') {
       csList.push(columns[0]);
     }
     if (columns[3] === "SWE") {
       sweList.push(columns[0]);
     }
   });
-  console.log("Number of students:", count - 1)
-  console.log(`Number of students in CS: ${csList.length}. List: ${csList.join(", ")}`);
-  console.log("Number of students in SWE: " +
-	  `${sweList.length}. List: ` + `${sweList.join(", ")}`);
+  console.log(`Number of students: ${count - 1}`)
+  console.log(`Number of students in CS: ${csList.length} List: ${csList.join(', ')}`);
+  console.log(`Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`);
 };
 
 module.exports = countStudents;
