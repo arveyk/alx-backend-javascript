a92ff1404e38e9b106a2026c96f53f8d1cc2cd9b
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-26 01:33:23 +0300

    Spies

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,6 +2,6 @@ const Utils = require('./utils.js');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const cost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${cost}`;
+  console.log(`The total is: ${cost}`);
 }
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,39 +1,49 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestApi = require('./3-payment.js');
-const assert = require('assert')
+const assert = require('assert');
 
 describe('sendPaymentRequestToApi', function() {
   it('should return total shipping const', function() {
     const spy = sinon.spy(Utils.calculateNumber);
-    const spy0 = sinon.spy(sendPaymentRequestApi);
+    assert.equals(sendPaymentRequestApi(1.2, 1.5), spy('SUM', 1.2, 1.5));
+    spy0.restore();
+  });
 });
-describe('calculateNumber', function () {
+describe('sendPaymentRequestToApi', function () {
   it('should return 3', function () {
     //assert.equals(sendPaymentRequestApi(1.2, 1.5), Utils.calculateNumber('SUM', 1.2, 1.5));
     const spy = sinon.spy(Utils.calculateNumber);
     const spy0 = sinon.spy(sendPaymentRequestApi);
     assert.equals(spy0(1.2, 1.5), spy('SUM', 1.2, 1.5));
+    spy.restore();
+    spy0.restore();
   });
 });
-describe('calculateNumber', function () {
+describe('sendPaymentRequestToApi', function () {
   it('should return 4', function () {
     const spy = sinon.spy(Utils.calculateNumber);
     const spy0 = sinon.spy(sendPaymentRequestApi);
-    assert.equals(spy0(7.8, 4), spy('SUM', 7.8, 4);
+    assert.equals(spy0(7.8, 4), spy('SUM', 7.8, 4));
+    spy.restore();
+    spy0.restore();
   });
 });
-describe('calculateNumber', function () {
+describe('sendPaymentRequestToApi', function () {
   it('should return 0.25', function () {
     const spy = sinon.spy(Utils.calculateNumber);
     const spy0 = sinon.spy(sendPaymentRequestApi);
     assert.equals(spy0(1, 3.7), spy('SUM', 1, 3.7));
+    spy.restore();
+    spy0.restore();
   });
 });
-describe('calculateNumber', function () {
+describe('sendPaymentRequestToApi', function () {
   it('should return 0.5', function () {
     const spy = sinon.spy(Utils.calculateNumber);
     const spy0 = sinon.spy(sendPaymentRequestApi);
     assert.equals(spy0(1, 1.5), spy('SUM', 1, 1.5));
+    spy.restore();
+    spy0.restore();
   });
 });
