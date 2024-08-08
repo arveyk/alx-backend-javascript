73d2a0c919750d5c936a840bd5f25111979482d0
arveyk <harveykisiangani@gmail.com>
2024-08-08 20:43:56 +0300

    ChatGpt suggestion

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,24 +2,13 @@ import { uploadPhoto, createUser } from './utils.js';
 
 
 function handleProfileSignup() {
-  let firstName;
-  let lastName;
-  let body;
-  const photot = uploadPhoto();
-   .then((res) => {
-      body = res['body'];
-    }).catch((error) => {
-      console.log('Signup system offline'});
-  createUser()
-    .then((res) => {
-      fistName = res['firstName'];
-      lastName = res['lastName'];
+  Promise.all([uploadPhoto(), createUser()])
+   .then((photoRes, userResp) => {
+     const { firstName, lastName } = userResp;
+     const { body } = photoRes;
+      console.log(`${body} ${fistName} ${lastName}`);
     }).catch((error) => {
       console.log('Signup system offline'});
     });
-  console.log(`${body} ${fistName} ${lastName}`);
-  return new Promise((resolve, reject) => {
-    resolve(`${body} ${fistName} ${lastName}`);
-    reject(new Error('Signup system offline'));
   }
 }
