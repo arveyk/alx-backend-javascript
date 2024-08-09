05015e572d819b2db4671165391304b7fe7f6757
arveyk <harveykisiangani@gmail.com>
2024-08-09 16:12:25 +0300

    Promise rejected

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,9 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, fileName) {
   const promise0 = signUpUser(firstName, lastName);
   const promise1 = uploadPhoto(fileName);
-  return Promise.allSettled([promise0, promise1]).then((results) => {
-    return [results];
-  });
+  return Promise.allSettled([promise0, promise1]).then((results) => results);
 }
 module.exports = handleProfileSignup;
