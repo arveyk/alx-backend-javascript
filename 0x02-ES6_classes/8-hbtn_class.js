46ffabe3d3031b4b981881da1ede5cc668270759
arveyk <harveykisiangani@gmail.com>
2024-08-12 19:02:32 +0300

    toString and valueOf methods

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,12 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+  toString() {
+    return this._location;
+  }
+  valueOf() {
+    return this._size;
+  }
+}
