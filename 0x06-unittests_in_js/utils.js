cf4323c3d9360bbee355fdf3a482fa6f3fba8586
arveyk <harveykisiangani@gmail.com>
2024-09-07 22:50:24 +0300

    Zero division error

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,11 +1,14 @@
 const Utils = {
   calculateNumber: (ops, a, b) => {
     let ret = 0;
-    switch (ops) {
+    /*switch (ops) {
       case 'SUM':
         ret = Math.round(a) + Math.round(b);
         break;
       case 'DIVIDE':
+        if (b === 0) {
+          return 'Error';
+        }
 	ret = Math.round(a) / Math.round(b);
         break;
       case 'SUBTRACT':
@@ -14,7 +17,23 @@ const Utils = {
       default:
         ret = 'Operation not found';
     }
+    */
+    if (ops === 'SUM') {
+      ret = Math.round(a) + Math.round(b);
+    }
+    if (ops === 'DIVIDE') {
+      if (b === 0) {
+        return 'Error';
+      }
+      ret = Math.round(a) / Math.round(b);
+    }
+    if (ops === 'SUBTRACT') {
+	ret = Math.round(a) - Math.round(b);
+    }
     return ret
+  },
+toLowerCase: (str) => {
+    return String.toLowerCase(str)
   }
 }
 module.exports = Utils;
